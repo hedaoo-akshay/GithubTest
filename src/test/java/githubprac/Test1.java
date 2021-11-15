a878a7ec31b671a675bcd72185faac466de5bee8
Moulibasha <moulibasha4@gmail.com>
2021-11-15 12:00:53 +0530

    Done by Mouly

--- src/test/java/githubprac/Test1.java
+++ src/test/java/githubprac/Test1.java
@@ -3,8 +3,9 @@ package githubprac;
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Test1");
+		System.out.println("Test1");
+		System.out.println("modified by Mouly");
+
 	}
 
 }
